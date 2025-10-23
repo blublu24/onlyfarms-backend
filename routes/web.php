d99d6e117134a153eb2c0886088153533b2eb026
blublu24 <bluebasco8@gmail.com>
2025-10-23 20:04:24 +0800

    Add test endpoints to debug storage issues

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/test-storage', function () {
+    $storagePath = storage_path('app/public');
+    $publicPath = public_path('storage');
+    
+    return response()->json([
+        'storage_path' => $storagePath,
+        'public_path' => $publicPath,
+        'storage_exists' => file_exists($storagePath),
+        'public_storage_exists' => file_exists($publicPath),
+        'storage_link_exists' => is_link($publicPath),
+        'files_in_storage' => file_exists($storagePath) ? array_slice(scandir($storagePath), 2, 10) : [],
+        'files_in_public_storage' => file_exists($publicPath) ? array_slice(scandir($publicPath), 2, 10) : [],
+    ]);
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test-image/{filename}', function ($filename) {
+    $storagePath = storage_path('app/public/products/' . $filename);
+    $publicPath = public_path('storage/products/' . $filename);
+    
+    return response()->json([
+        'filename' => $filename,
+        'storage_path' => $storagePath,
+        'public_path' => $publicPath,
+        'storage_file_exists' => file_exists($storagePath),
+        'public_file_exists' => file_exists($publicPath),
+        'storage_file_size' => file_exists($storagePath) ? filesize($storagePath) : 0,
+        'public_file_size' => file_exists($publicPath) ? filesize($publicPath) : 0,
+    ]);
 });
