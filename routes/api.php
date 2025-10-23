a06e518a79d1a7c99b469caf6bd06ff4b20fafe3
blublu24 <bluebasco8@gmail.com>
2025-10-23 20:05:42 +0800

    Add storage debug endpoints to API routes

--- routes/api.php
+++ routes/api.php
@@ -271,6 +271,37 @@ Route::post('/debug/send-test-email', function (\Illuminate\Http\Request $reques
     }
 });
 
+// Debug routes for storage issues
+Route::get('/test-storage', function () {
+    $storagePath = storage_path('app/public');
+    $publicPath = public_path('storage');
+    
+    return response()->json([
+        'storage_path' => $storagePath,
+        'public_path' => $publicPath,
+        'storage_exists' => file_exists($storagePath),
+        'public_storage_exists' => file_exists($publicPath),
+        'storage_link_exists' => is_link($publicPath),
+        'files_in_storage' => file_exists($storagePath) ? array_slice(scandir($storagePath), 2, 10) : [],
+        'files_in_public_storage' => file_exists($publicPath) ? array_slice(scandir($publicPath), 2, 10) : [],
+    ]);
+});
+
+Route::get('/test-image/{filename}', function ($filename) {
+    $storagePath = storage_path('app/public/products/' . $filename);
+    $publicPath = public_path('storage/products/' . $filename);
+    
+    return response()->json([
+        'filename' => $filename,
+        'storage_path' => $storagePath,
+        'public_path' => $publicPath,
+        'storage_file_exists' => file_exists($storagePath),
+        'public_file_exists' => file_exists($publicPath),
+        'storage_file_size' => file_exists($storagePath) ? filesize($storagePath) : 0,
+        'public_file_size' => file_exists($publicPath) ? filesize($publicPath) : 0,
+    ]);
+});
+
 // ==================== END DEBUG ENDPOINTS ====================
 
 use App\Http\Controllers\AuthController;
